c92dc4b30bc007306fc4d9d3cf1aa5eb27cb1b88
shai9 <dennis.shakava@student.moringaschool.com>
2022-09-23 10:10:37 +0300

    Done

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -27,3 +27,40 @@ RSpec.describe Student, type: :model do
     end
   end
 end
+
+it 'returns the second student' do
+  get "/students/#{second_student.id}"
+
+  expect(response.body).to include_json({
+    id: a_kind_of(Integer),
+    first_name: 'Idris', 
+    last_name: 'Elba', 
+    grade: 105
+  })
+end
+end
+
+describe "GET /students" do
+it 'returns an array of all students' do
+  get '/students'
+
+  expect(response.body).to include_json([
+    { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
+    { first_name: 'Idris', last_name: 'Elba', grade: 105 },
+    { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
+    { first_name: 'Jason', last_name: 'Statham', grade: 70 }
+  ])
+end
+
+context 'with query params' do
+  it 'returns students whose first or last name matches the query' do
+    get '/students?name=kirby'
+
+    expect(response.body).to include_json([
+      { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 }
+    ])
+  end
+end
+end
+
+end
